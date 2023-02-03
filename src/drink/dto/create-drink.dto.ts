fd700302822172e1ff9f378c89ce733ac708326e
Yuriy <batm1x1@gmail.com>
2023-02-04 02:54:16 +0300

    add model

---
+++ src/drink/dto/create-drink.dto.ts
@@ -0,0 +1,16 @@
+import { Type } from '@nestjs/class-transformer';
+import { PriceType } from '../schema/drink.schema';
+
+class Price {
+  readonly ml50: number;
+  readonly ml100: number;
+  readonly bottle: number;
+}
+
+export class CreateDrinkDto {
+  readonly name: string;
+  readonly country: string;
+  @Type(() => Price)
+  readonly price: Price;
+  readonly specification;
+}
