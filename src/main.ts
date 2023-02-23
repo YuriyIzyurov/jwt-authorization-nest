4a3e5597cff7a371f089e7aaaac26ef430c8c4be
Yuriy <batm1x1@gmail.com>
2023-02-23 17:22:53 +0300

    add CORS options

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
 async function start() {
   try {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule, { cors: true });
-    app.enableCors();
+    app.enableCors(corsOptions);
     await app.listen(PORT, () => console.log(`server is up on PORT ${PORT}`));
   } catch (e) {
     console.log(e);
