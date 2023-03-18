0bd00e285cb737a1e40223e6bd3e954c52d5b402
Yuriy <batm1x1@gmail.com>
2023-03-18 15:48:47 +0300

    add  policy

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const corsOptions = {
-  origin: 'http://localhost:3000',
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-
 async function start() {
   try {
     const PORT = process.env.PORT || 5000;
-    const app = await NestFactory.create(AppModule, { cors: true });
-    app.enableCors(corsOptions);
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({
+      origin: 'https://cafe-lemon.vercel.app',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+      credentials: true,
+    });
     await app.listen(PORT, () => console.log(`server is up on PORT ${PORT}`));
   } catch (e) {
     console.log(e);
