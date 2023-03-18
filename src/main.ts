dd3103202e71026ec49ca11a8c4e1f281c7415c3
Yuriy <batm1x1@gmail.com>
2023-03-18 21:10:38 +0300

    allow default CORS

--- src/main.ts
+++ src/main.ts
@@ -9,20 +9,19 @@ async function start() {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule);
     const corsOptions = {
-      // origin: [process.env.ALLOWED_CLIENT_URL],
-      origin: '*',
+      origin: [process.env.ALLOWED_CLIENT_URL],
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      preflightContinue: true,
+      preflightContinue: false,
       optionsSuccessStatus: 204,
       credentials: true,
       allowedHeaders: ['Accept', 'Content-Type'],
       allowedMethods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     };
     if (isDev) {
-      // corsOptions.origin.push(process.env.ALLOWED_CLIENT_LOCALHOST);
+      corsOptions.origin.push(process.env.ALLOWED_CLIENT_LOCALHOST);
     }
 
-    app.enableCors(corsOptions);
+    app.enableCors();
 
     await app.listen(PORT, () => console.log(`server is up on PORT ${PORT}`));
   } catch (e) {
