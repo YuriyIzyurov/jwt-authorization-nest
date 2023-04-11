0aaffb047eff8882b1a5d89313db448e110e7f54
Yuriy <batm1x1@gmail.com>
2023-04-11 20:13:48 +0300

    add path

--- src/review/dto/create-review.dto.ts
+++ src/review/dto/create-review.dto.ts
@@ -1,6 +1,8 @@
 import {IsOptional} from "class-validator";
 
-
+//todo: почему не работает не isoptional message
+// :
+// ["an unknown value was passed to the validate function"]
 export class CreateReviewDto {
   @IsOptional()
   name: string;
